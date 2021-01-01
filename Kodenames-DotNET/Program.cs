cdf91bbf937895b7712952278c1320425e90f75e
Akash Bakshi-GamingPC <akash1996@hotmail.com>
2021-01-01 15:22:53 -0500

    - Modified dockerfile to include correct urls so it works

--- Kodenames-DotNET/Program.cs
+++ Kodenames-DotNET/Program.cs
@@ -20,7 +20,7 @@ namespace Kodenames_DotNET
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();   
                 });
     }
 }
